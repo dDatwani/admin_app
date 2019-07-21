c3b5a973138aef8aa33de849d460503d0c1f1d6f
deepak-datwani <dep.light@gmail.com>
2019-07-22 01:28:54 +0530

    use of sendgrid

--- common/mailer.js
+++ common/mailer.js
@@ -1,4 +1,5 @@
 var nodemailer = require("nodemailer");
+var helper = require('sendgrid').mail;
 let testAccount = await nodemailer.createTestAccount();
 var smtpTransport = nodemailer.createTransport({
     service: 'gmail',
@@ -26,23 +27,30 @@ function mail(messageBody) {
      });
 }
 function forgotPasswordMail(messageBody, to) {
-    let messageBodyJson = JSON.stringify(messageBody)
+    let messageBodyJson = JSON.stringify(messageBody);
+    var from_email = new helper.Email('mailtodeepak.me@gmail.com');
+    var to_email = new helper.Email(to);
+    var subject = 'Forgot Password request from admin-app';
+    var content = new helper.Content('text/plain', messageBody);
+    var mail = new helper.Mail(from_email, subject, to_email, content);
+    var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+    var request = sg.emptyRequest({
+    method: 'POST',
+    path: '/v3/mail/send',
+    body: mail.toJSON(),
+    });
     return new Promise( (resolve, reject)=> {
-        smtpTransport.sendMail({  //email options
-            from: "mailtodeepak.me@gmail.com", // sender address.  Must be the same as authenticated user if using Gmail.
-            to: to, // receiver
-            subject: "Forgot Password request from admin-app", // subject
-            text: messageBodyJson // body
-         }, function(error, response){  //callback
-            if(error){
-               console.log("error",error);
-               reject(error)
-            }else{
-                console.log(response);
+        sg.API(request, function(error, response) {
+            if(error) {
+                reject(error);
+            }else {
+                console.log(response.statusCode);
+                console.log(response.body);
+                console.log(response.headers);
                 resolve(response);
             }
-         //    smtpTransport.close(); // shut down the connection pool, no more messages.  Comment this line out to continue sending emails.
-         });
+            
+          });
     });
     
 }
